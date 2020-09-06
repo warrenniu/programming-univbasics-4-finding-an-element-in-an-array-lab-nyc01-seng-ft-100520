60baf7812fd0e06f268aa9b57f68ce3c3d63562b
Warren Niu <warrenniu@gmail.com>
2020-09-06 15:05:41 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   found_value_index = nil
   while i < array.length do
     if array[i] == value_to_find
-      found_value_index == i
+      found_value_index = i
     end
     i += 1
   end
